5509d5f79d7695762f041d8cf256fd0b7cc1c11c
Rakshitha B A <rakshitha.aruna@gmail.com>
2023-02-05 16:38:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function menu(){
+    if(document.getElementById('menu').style.display == 'block'){
+        document.getElementById('menu').style.display = 'none';
+    }
+    else{
+        document.getElementById('menu').style.display = 'block';
+    }
+}
